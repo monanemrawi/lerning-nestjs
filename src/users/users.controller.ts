3b107094a63b56fc208542f1618f46865bd76e50
mona <monanemrawi@gmail.com>
2024-03-30 18:11:32 +0300

    created user routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() // GET /users or /users?role=value
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get(':id') // GET /users/id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/id
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /user/id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
